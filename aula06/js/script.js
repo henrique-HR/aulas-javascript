b1cf36f5721a66a49bb44da14414f89d68607eeb
henrique-HR <99933690+henrique-HR@users.noreply.github.com>
2022-08-13 14:37:47 -0300

    novas-aulas

---
+++ aula06/js/script.js
@@ -0,0 +1,26 @@
+
+
+/*var itens0= document.querySelectorAll('#lista2 li')[0]
+var itens1= document.querySelectorAll('#lista2 li')[1]
+var itens2= document.querySelectorAll('#lista2 li')[2]
+var itens3= document.querySelectorAll('#lista2 li')[3]
+var itens4= document.querySelectorAll('#lista2 li')[4]
+var itens5= document.querySelectorAll('#lista2 li')[5]
+var itens6= document.querySelectorAll('#lista2 li')[6]
+var itens7= document.querySelectorAll('#lista2 li')[7]
+/*itens0.innerText ='feijao'
+itens1.innerText ='arroz'
+itens2.innerText ='batata'
+itens3.innerText ='carne'
+itens4.innerText ='macarrao'
+itens5.innerText ='leite'
+itens6.innerText ='cafe'
+itens7.innerText ='acucar'*/
+itens= document.querySelectorAll('#lista2 li')
+
+
+    
+
+
+
+       
